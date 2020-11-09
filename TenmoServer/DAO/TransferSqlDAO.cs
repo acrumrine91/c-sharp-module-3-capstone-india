76d891127802d0274dfaefa9e38425c8e08870f7
Austin <Acrumrine91@yahoo.com>
2020-11-09 01:48:22 -0500

    trying to get our working transfer

--- TenmoServer/DAO/TransferSqlDAO.cs
+++ TenmoServer/DAO/TransferSqlDAO.cs
@@ -18,101 +18,37 @@ namespace TenmoServer.DAO
             connectionString = dbConnectionString;
         }
 
-        //public bool TransferFunds(int userTo, int userFrom, decimal amount)
-        //{
-        //    int accountTo = GetAccountId(userTo);
-        //    int accountFrom = GetAccountId(userFrom);
-
-        //    bool successful = false;
-        //    try
-        //    {
-        //        using (SqlConnection conn = new SqlConnection(connectionString))
-        //        {
-        //            conn.Open();
-        //            SqlCommand command = new SqlCommand("INSERT INTO transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) VALUES (@type, @status, @from, @to, @amount)", conn);
-        //            command.Parameters.AddWithValue("@type", 1001);
-        //            command.Parameters.AddWithValue("@status", 2001);
-        //            command.Parameters.AddWithValue("@from", accountFrom);
-        //            command.Parameters.AddWithValue("@to", accountTo);
-        //            command.Parameters.AddWithValue("@amount", amount);
-        //            command.ExecuteNonQuery();
-
-        //            //command = new SqlCommand("SELECT @@IDENTITY", conn);
-        //            successful = true;
-        //            return successful;
-        //        }
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        return successful;
-        //    }
-
-        //}
-        public int GetAccountId(int user_id)
+        public bool TransferFunds(int accountTo, int accountFrom, decimal amount)
         {
-            int result = -1;
+            bool successful = false;
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
-
-                    SqlCommand cmd = new SqlCommand("SELECT account_id FROM accounts WHERE user_id = @userId", conn);
-                    cmd.Parameters.AddWithValue("@userId", user_id);
-
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        result = Convert.ToInt32(reader["account_id"]);
-                    }
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                return result;
-            }
-        }
-
-        public Transfer TransferFunds(Transfer transfer)
-        {
-            int accountTo = GetAccountId(transfer.AccountTo);
-            int accountFrom = GetAccountId(transfer.AccountFrom);
-
-
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand command = new SqlCommand("INSERT INTO transfers VALUES (@type, @status, @from, @to, @amount); SELECT SCOPE_IDENTITY();", conn);
-                    command.Parameters.AddWithValue("@type", 1001);
-                    command.Parameters.AddWithValue("@status", 2001);
+                    SqlCommand command = new SqlCommand("INSERT INTO transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) VALUES (@type, @status, @from, @to, @amount)", conn);
+                    command.Parameters.AddWithValue("@type", 2);
+                    command.Parameters.AddWithValue("@status", 2);
                     command.Parameters.AddWithValue("@from", accountFrom);
                     command.Parameters.AddWithValue("@to", accountTo);
-                    command.Parameters.AddWithValue("@amount", transfer.Amount);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.ExecuteNonQuery();
 
-
-                    int id = Convert.ToInt32(command.ExecuteScalar());
-                    return new Transfer
-                    {
-                        TransferID = id,
-                        TransferType = 1001,
-                        TransferStatus = 2001,
-                        Amount = transfer.Amount,
-                        AccountFrom = accountFrom,
-                        AccountTo = accountTo
-                    };
-
+                    command = new SqlCommand("SELECT @@IDENTITY", conn);
+                    successful = true;
+                    return successful;
                 }
-
-
             }
-
+            catch (SqlException ex)
+            {
+                return successful;
+            }
 
         }
 
-
     }
+
+
 }
 
 
