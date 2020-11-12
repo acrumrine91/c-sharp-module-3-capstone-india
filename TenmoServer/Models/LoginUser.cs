51a584bc762fb8860bc5ddc464b1f57ee6cc28fe
Kip <lexnercj@gmail.com>
2020-11-11 22:55:08 -0500

    FINAL VERSION OF CAPSTONE FOR MATT

--- TenmoServer/Models/LoginUser.cs
+++ TenmoServer/Models/LoginUser.cs
@@ -6,11 +6,8 @@ namespace TenmoServer.Models
     /// Model to accept login parameters
     /// </summary>
     public class LoginUser
-<<<<<<< HEAD
-    { 
-=======
-    {
->>>>>>> 77161be2034ad7214800b53807a894cb40f16038
+
+    {   
     [Required]
         public string Username { get; set; }
         [Required]
